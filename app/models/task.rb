cf1f439d0f338cfeeb5aec3080af702ad19ec9c7
Aevamcollage <230301143@darshan.ac.in>
2026-05-25 00:01:25 +0530

    Refactor views for improved UI/UX consistency and responsiveness
    - Updated login and signup forms with new styling for better user experience.
    - Enhanced task forms and index pages with improved layouts and visual hierarchy.
    - Redesigned task detail view to include a more structured layout for item information and updates.
    - Improved error handling display in forms for better visibility.
    - Adjusted button styles and interactions for a more modern look and feel.

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,5 @@ class Task < ApplicationRecord
   validates :title, presence: {message:"Did you forgot to add Title?"}
   validates :description, presence: {message:"ooops! You forgot to add description..."}
   enum :status, [ :inprogress, :archived ,:done] ,default: :inprogress
-  enum :priority, [ :high, :low ,:urgent] ,default: :low
+  enum :priority, { high: 0, low: 1, urgent: 2, medium: 3 }, default: :low
 end
